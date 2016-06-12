072b46eaef6305c749bc563054997f8466b660a4
smartie007up <Sinaasappel1>
2016-06-12 17:59:01 +0200

    adjusted

--- target_selection/clickon.js
+++ target_selection/clickon.js
@@ -1,23 +1,14 @@
-//handling making the csv
-var container = document.querySelector("body");
+// var container = document.querySelector("body");
+//
+// window.onload=function(){
+//   container.addEventListener("click", getClickPosition, false);
+// }
 
-window.onload=function(){
-  container.addEventListener("click", getClickPosition, false);
-
-}
-var count = 0;
 function getClickPosition(e){
-    count += 1;
-    if (count > 5) {
-
-      console.log("done!")
-    }
-
   var parentPosition = getPosition(container);
   var x = event.clientX - parentPosition.x;     // Get the horizontal coordinate
   var y = event.clientY - parentPosition.y;     // Get the vertical coordinate
   console.log(x, y);
-
 }
 
 function getPosition(el) {
@@ -43,55 +34,4 @@ function getPosition(el) {
     x: xPosition,
     y: yPosition
   }
-}
-
-function convertArrayOfObjectsToCSV(args) {
-  var result, ctr, keys, columnDelimiter, lineDelimiter, data;
-
-  data = args.data || null;
-  if (data == null || !data.length) {
-    return null;
-  }
-
-  columnDelimiter = args.columnDelimiter || ',';
-  lineDelimiter = args.lineDelimiter || '\n';
-
-  keys = Object.keys(data[0]);
-
-  result = '';
-  result += keys.join(columnDelimiter);
-  result += lineDelimiter;
-
-  data.forEach(function(item) {
-    ctr = 0;
-    keys.forEach(function(key) {
-      if (ctr > 0) result += columnDelimiter;
-
-      result += item[key];
-      ctr++;
-    });
-    result += lineDelimiter;
-  });
-
-  return result;
-}
-
-function downloadCSV(args) {
-  var data, filename, link;
-  var csv = convertArrayOfObjectsToCSV({
-    data: window.x
-  });
-  if (csv == null) return;
-
-  filename = args.filename || 'export.csv';
-
-  if (!csv.match(/^data:text\/csv/i)) {
-    csv = 'data:text/csv;charset=utf-8,' + csv;
-  }
-  data = encodeURI(csv);
-
-  link = document.createElement('a');
-  link.setAttribute('href', data);
-  link.setAttribute('download', filename);
-  link.click();
 }
--- target_selection/random_position.js
+++ target_selection/random_position.js
@@ -1,31 +1,74 @@
-var random = Math.floor((Math.random() * $('.blocks').size()));
-$('.blocks').hide().eq(random).show();
+var container = document.querySelector("body");
 
-// var arr = $(".blocks").toArray();
-// var item = $(this).attr('id');
+window.onload=function(){
+  hide(document.querySelectorAll('.blocks'));
+  document.body.addEventListener("click", onClick);
+  //container.addEventListener("click", getClickPosition, false);
+}
+var xpos, ypos;
+var count = 0;
 
+function hide (elements) {
+  var random = Math.floor((Math.random() * $('.blocks').size()));
+  elements = elements.length ? elements : [elements];
+  for (var index = 0; index < elements.length; index++) {
+    elements[index].style.display = 'none';      // Hide
+    elements[random].style.display = 'block';     // Show
+  }
+  var d = getPosition(elements[random]);
+  if(count < 5){
+    places(d);
+  }
+  if (count >= 5){
+    replaces(d);
+  }
+}
 
-// var myElement = document.querySelector(item);
-// var position = getPosition(myElement);
-var x, y;
-
-$( "div" ).click(function() {
-  d = $(this).position();
-  getPosition(d);
-  xpos = d.left;
-  ypos = d.top;
+function places(r){
+  xpos = r.x + 4;
+  ypos = r.y + 4;
+  var parentPosition = getPosition(container);
+  var x = event.clientX - parentPosition.x;     // Get the horizontal coordinate
+  var y = event.clientY - parentPosition.y;     // Get the vertical coordinate
+  console.log(x, y);
   console.log(xpos, ypos);
-  location.reload();
+  // a = x - xpos;
+  // b = y - ypos;
+  // console.log(a,b);
+}
+
+function replaces(r){
+  xpos = r.x + 7;
+  ypos = r.y + 7;
+  var parentPosition = getPosition(container);
+  var x = event.clientX - parentPosition.x;     // Get the horizontal coordinate
+  var y = event.clientY - parentPosition.y;     // Get the vertical coordinate
+  console.log(x, y);
+  console.log(xpos, ypos);
+  // a = x - xpos;
+  // b = y - ypos;
+  // console.log(a,b);
+}
+
+$( ".blocks" ).click(function() {
+  hide(document.querySelectorAll('.blocks'));
 });
 
+var onClick = (function(){
+  return function(){
+    count++;
+    if( count >= 5 ){
+      var x = document.getElementsByClassName("blocks");
+      var i;
+      for (i = 0; i < x.length; i++) {
+        x[i].style.width = "12px";
+        x[i].style.height = "12px";
+        x[i].style.border = "14px solid red";
+      }
+    }
+    if( count >= 10 ){
 
-// function getOffset( el ) {
-//   var _x = 0;
-//   var _y = 0;
-//   while( el && !isNaN( el.offsetLeft ) && !isNaN( el.offsetTop ) ) {
-//     _x += el.offsetLeft - el.scrollLeft;
-//     _y += el.offsetTop - el.scrollTop;
-//     el = el.offsetParent;
-//   }
-//   return { top: _y, left: _x };
-// }
+      //  location.replace(""); //terug
+    }
+  }
+})();
