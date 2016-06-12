e58f56ca538e96c1927caf81fadeef425edf4b2d
smartie007up <Sinaasappel1>
2016-06-12 23:10:41 +0200

    klaarhoopik

--- target_selection/random_position.js
+++ target_selection/random_position.js
@@ -5,20 +5,9 @@ window.onload=function(){
   container.addEventListener("click", onClick);
 }
 
-var xpos, ypos; //position of the random block
-var count = 0; //counter for the number of tests
-var randompos; //the random block
-var clickx = 0; //the x click position
-var clicky = 0; //the y click position
-var errorx, errory; //the amount by which it is off
-
 function hide (elements) {
   errorx = clickx - xpos;
   errory = clicky - ypos;
-  console.log(errorx, errory);
-
-  //  saveCsv(errorx, errory);
-
   var random = Math.floor((Math.random() * $('.blocks').size()));
   elements = elements.length ? elements : [elements];
   for (var index = 0; index < elements.length; index++) {
@@ -32,21 +21,29 @@ function hide (elements) {
 $( ".blocks" ).click(function() {
   parentpos();
   hide(document.querySelectorAll('.blocks'));
+  Good.push(1);
+  Rows.push("Block");
+  Xerror.push(errorx);
+  Yerror.push(errory);
+  console.log(Rows,Xerror, Yerror, Good);
 });
 
 function places(){
   xpos = randompos.x + 4;
   ypos = randompos.y + 4;
 }
-
-$( "#middle_click" ).click(function() {
-  parentpos();
-  var randomp = getPosition(this);
-  var t = randomp.x + 4;
-  var g = randomp.y + 4;
-  places();
-  errorx = clickx - t;
-  errory = clicky - g;
-
-  console.log("middle:",errorx, errory);
-});
+//
+// $( "#middle_click" ).click(function() {
+//   parentpos();
+//   var randomp = getPosition(this);
+//   var t = randomp.x + 4;
+//   var g = randomp.y + 4;
+//   places();
+//   errorx = clickx - t;
+//   errory = clicky - g;
+//   Good.push(1);
+//   Rows.push("Middle");
+//   Xerror.push(errorx);
+//   Yerror.push(errory);
+//   console.log(Rows,Xerror, Yerror, Good);
+// });
